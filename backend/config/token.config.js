37fc0cd796447b093054a350779d8bbc7ba93dc5
CodingError7 <144795507+codingerror7@users.noreply.github.com>
2026-03-17 18:26:05 +0530

    Fix token expiration format in token.config.js

--- backend/config/token.config.js
+++ backend/config/token.config.js
@@ -1,7 +1,7 @@
 import jwt from "jsonwebtoken";
 const genToken = async (userId) => {
     try {
-        let token = await jwt.sign({userId},process.env.JWT_SIGN,{expiresIn : "7days"});
+        let token = await jwt.sign({userId},process.env.JWT_SIGN,{expiresIn : "7d"});
         return token;
     } catch (error) {
         console.log(error);
